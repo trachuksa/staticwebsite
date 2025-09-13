b6ccd8c02206cdd28f65131a0d28c1a7c1bdcf1e
Svetlana <trachuksa@gmail.com>
2025-09-13 15:31:48 +0200

    picture

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function MyButton() {
     </button>
   );
 }
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
 
 function App() {
@@ -41,8 +46,25 @@ function App() {
           <h1>Welcome to my app</h1>
           <MyButton />
       </div>
+      <div>
+          <h1>{user.name}</h1>
+          <img
+            className="avatar"
+           src={user.imageUrl}
+            alt={'Photo of ' + user.name}
+            style={{
+              width: user.imageSize,
+              height: user.imageSize
+           }}
+          />
+      </div>
+          
     </>
   )
 }
+
+
+
+
 
 export default App
