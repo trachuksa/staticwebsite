3c093229fe47071e81fa4889be112fa1e3dcc648
Svetlana <trachuksa@gmail.com>
2025-09-13 15:09:18 +0200

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +37,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more And Svetlana 
       </p>
+      <div>
+          <h1>Welcome to my app</h1>
+          <MyButton />
+      </div>
     </>
   )
 }
